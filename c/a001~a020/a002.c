aef959146262ded17c831a50aa4ee2e750c167fa
taeyeonssupdate <mfpss96156@gmail.com>
2021-08-19 00:15:35 +0800

    add a002

---
+++ c/a001~a020/a002.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int x, y;
+    scanf("%d %d", &x, &y);
+    printf("%d", x + y);
+    return 0;
+}
---
+++ java/a001~a020/a002.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class a002 {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int x, y;
+        x = sc.nextInt();
+        y = sc.nextInt();
+        System.out.println(x + y);
+        sc.close();
+    }
+}
